14676fc790cf21ed0fdf5f09e05201e64047ee1d
natfi <flopyfida@gmail.com>
2021-09-04 23:47:58 -0300

    details

--- js/desconectar.js
+++ js/desconectar.js
@@ -1,7 +1,7 @@
 function desconectar() {
 	localStorage.removeItem("usuario");
-	signOut();
 	location.href = "login.html";
+	signOut();
 }
 
 
@@ -16,12 +16,12 @@ document.addEventListener("DOMContentLoaded", function(e){
 				width: '461px',
 		    	//html:'<b>Ingresa para continuar al sitio</b>',
 			    background:'linear-gradient(to top, rgb(234, 255, 239), rgba(255,255,255,0))',
-			    imageUrl: '/img/jap.png',
+			    imageUrl: '../img/jap.png',
 			    imageWidth:'90px',
 			    imageHeight:'70px',
 			    backdrop: `
-			    rgba(0,0,173,0.4)
-			    url("/img/sa.gif")
+			    rgba(129, 208, 248, 0.781)
+			    url("../img/sa.gif")
 			    top
 			    no-repeat
 		        `,
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function(e){
 			    hideClass: {
 			    	popup: 'animate__animated animate__fadeOutUp'
 			    },
-				confirmButtonColor: 'green',
+				confirmButtonColor: 'rgba(6, 169, 250, 0.781)',
 			    confirmButtonText: 'Ingresa para continuar al sitio',
 			}).then((result) => {
 				if (result.isConfirmed) {
