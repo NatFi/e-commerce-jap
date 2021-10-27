913e8b38c7b18207c323ba1e44249a144d35c50c
natfi <flopyfida@gmail.com>
2021-10-27 01:14:22 -0300

    perfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -27,16 +27,26 @@ document.addEventListener("DOMContentLoaded", function (e) {
     });
 
 
-
-
-    let imgbase64 = convertir(document.getElementById("img"));
-    localStorage.imagen=imgbase64;
-    document.getElementById("imagen2").src=`${localStorage.imagen}`;
-    console.log(localStorage.imagen);
+    /*
+    var imgbase64 = convertir(document.getElementById("img"));
+    localStorage.profileImg = imgbase64;
+    document.getElementById("img2").src=`${localStorage.profileImg}`; // ME PONE LA IMAGEN EN OTRO SITIO CON EL ID IMG2 por ejemplo;
+    console.log(localStorage.imgbase64);
     console.log(imgbase64);
+    */
 
+    let preview = document.getElementById("foto");
+    if (perfil != null){
+      document.getElementById("foto").src = perfil.imagen;
+    } else {
+      preview.src = "../img/jap.png";
+    }
+   
+
+   
 });
 
+ /*** *** *** *** *** *** *** *** *** *** *** *** *** ***/  
 
 
 function guardarPerfil() {
@@ -46,6 +56,7 @@ function guardarPerfil() {
     let edad = document.getElementById("edad");
     let email = document.getElementById("email");
     let tel = document.getElementById("tel");
+    let preview = document.getElementById("foto");
 
     let perfil = {};
     perfil.nombre = name.value;
@@ -53,16 +64,12 @@ function guardarPerfil() {
     perfil.edad = edad.value;
     perfil.email = email.value;
     perfil.tel = tel.value;
+    perfil.imagen = preview.src;
+   
+    localStorage.setItem('usuario', JSON.stringify(perfil));
+    //localStorage.setItem("perfil", JSON.stringify(perfil));
 
-    localStorage.setItem("perfil", JSON.stringify(perfil));
-
-
-    let disabledInputs = document.getElementsByName("info");
-    for(let i=0; i < disabledInputs.length; i++){
-        disabledInputs[i].disabled = true;
-    };
-
-
+    /* alert */
     const Toast = Swal.mixin({
         background: 'linear-gradient(rgba(128, 128, 128, 0.377) ,rgba(6, 104, 128, 0.185))',
         iconColor: 'rgba(6, 103, 128, 0.897)',
@@ -76,25 +83,52 @@ function guardarPerfil() {
           toast.addEventListener('mouseenter', Swal.stopTimer)
           toast.addEventListener('mouseleave', Swal.resumeTimer)
         }
-      })
-      
-      Toast.fire({
-        icon: 'success',
-        html:'<h6 style="color: rgba(6, 103, 128, 0.897); position: absolute;  padding-left: 17px;">Información actualizada</h6>',
-      });
+    })
+    Toast.fire({
+      icon: 'success',
+      html:'<h6 style="color: rgba(6, 103, 128, 0.897); position: absolute;  padding-left: 17px;">Información actualizada</h6>',
+    });
+
+    /* deshabilitar inputs  */
+    let disabledInputs = document.getElementsByName("info");
+    for(let i=0; i < disabledInputs.length; i++){
+        disabledInputs[i].disabled = true;
+    };
     
-}
+};
 
-
-
+/*
 function convertir(img) {
     img.crossOrigin="anonymous";
-    let canvas = document.createElement("canvas");
+    var canvas = document.createElement("canvas");
     canvas.width = img.width;
     canvas.height = img.height;
-    let contexto = canvas.getContext("2d");
+    var contexto = canvas.getContext("2d");
     contexto.drawImage(img, 0, 0,img.width, img.height);
-    let dataURL = canvas.toDataURL("image/png"); //jpng
+    var dataURL = canvas.toDataURL("image/jpeg",0.5);
     return dataURL;
-}
+};
+*/
+
+function previewFile() {
+    let preview = document.getElementById('foto');
+    let file    = document.querySelector('input[type=file]').files[0];
+    let reader  = new FileReader();
+  
+    reader.onloadend = function () {
+      preview.src = reader.result; 
+      console.log(reader.result);
+    }
+  
+    if (file) {
+      reader.readAsDataURL(file);
+     
+    } else {
+      preview.src = "../img/jap.png";
+    }
+  }
+
+
+
+
 
